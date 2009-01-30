e10ad91aaba604dfbd69aeddaaf95d35252d4cc9
Przemysław Wesołek <jest@go.art.pl>
2009-01-30 12:56:04 +0000

    Moved Data::Pareto code to a module-like structure.

--- lib/Data/Pareto.pm
+++ lib/Data/Pareto.pm
@@ -4,76 +4,160 @@ use 5.006000;
 use strict;
 use warnings;
 
-require Exporter;
-use AutoLoader qw(AUTOLOAD);
-
-our @ISA = qw(Exporter);
-
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use Data::Pareto ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our @EXPORT = qw(
-	
-);
-
 our $VERSION = '0.01';
 
+sub new {
+	my ($class, $attrs) = @_;
+	my $self = { %$attrs };
+	$self->{pareto} = [ ];
+	$self->{vectorStatus} = { };
+	return bless $self, $class;
+}
 
-# Preloaded methods go here.
+sub add {
+	my ($self, $v) = @_;
+	$self->updatePareto($v);	
+}
 
-# Autoload methods go after =cut, and are processed by the autosplit program.
+sub addAll {	
+	my ($self, $allV) = @_;
+	$self->updatePareto($_) for @$allV;
+}
+
+sub getPareto {
+	my ($self) = @_;
+	return $self->{pareto};
+}
+
+sub updatePareto {
+	my ($self, $NV) = @_;
+	
+	# check if we already have a duplicate?
+	if ($self->hasDuplicates($NV)) {
+		# ...then it depends on the policy
+		if ($self->{duplicates}) {
+			# add the duplicated vector to the pareto set
+			push @{$self->{pareto}}, $NV;
+		} else {
+			# simply disgard the new vector
+		}
+		return;
+	}
+	
+	my @newP = ( );
+	my $surePareto = 0;
+	
+	# check with every vector considered pareto so far
+	for my $o (@{$self->{pareto}}) {
+		if ($surePareto) {
+			# preserve the current vector only if it is not dominated by new (now Pareto) vector
+			if ($self->isDominated($o, $NV)) {
+				$self->banVector($o);
+			} else {
+				push @newP, $o;
+			}
+		} else {
+			# stop processing with unchanged Pareto set if the new vector is dominated by the current one
+			return if $self->isDominated($NV, $o);
+			
+			# mark new vector as "sure Pareto" only if it dominates the current vector
+			if ($self->isDominated($o, $NV)) {
+				$surePareto = 1;
+				# ...and hence we don't preserve the dominated current vector
+				$self->banVector($o);
+				next;
+			}
+			
+			# otherwise, the current vector is for sure Pareto still, so preserve it
+			push @newP, $o;
+		}
+	}
+
+	push @newP, $NV;
+	$self->markVector($NV);
+	$self->{pareto} = \@newP;
+}
+
+sub isDominated {
+	my ($self, $dominated, $by) = @_;
+	for my $c (@{$self->{cols}}) {
+		return 0 if $dominated->[$c] < $by->[$c];
+	}
+
+	1;
+}
+
+sub vectorKey {
+	my ($self, $v) = @_;
+	my @cols = ( );
+	for my $c (@{$self->{cols}}) {
+		push @cols, $v->[$c];
+	}
+	
+	return join ';', @cols;
+}
+
+sub hasDuplicates {
+	my ($self, $v) = @_;
+	my $key = $self->vectorKey($v);
+	return (exists $self->{vectorStatus}{$key} && $self->{vectorStatus}{$key} > 0);
+}
+
+sub banVector {
+	my ($self, $v) = @_;
+	my $key = $self->vectorKey($v);
+	$self->{vectorStatus}{$key} = 0;
+}
+
+sub markVector {
+	my ($self, $v) = @_;
+	my $key = $self->vectorKey($v);
+	$self->{vectorStatus}{$key} = 1;
+}
 
 1;
 __END__
-# Below is stub documentation for your module. You'd better edit it!
 
 =head1 NAME
 
-Data::Pareto - Perl extension for blah blah blah
+Data::Pareto - Selecting Pareto sets in Perl
 
 =head1 SYNOPSIS
 
   use Data::Pareto;
-  blah blah blah
+  my $set = new Data::Pareto(cols => [0, 2]);
+  $set->add_all([
+      [ 5, "pareto", 10, 11 ],
+      [ 5, "dominated", 11, 9 ],
+      [ 4, "pareto2", 12, 12 ] 
+  ]);
+  
+  # returns [ [ 5, "pareto", 10, 11 ], [ 4, "pareto2", 12, 12 ] ]
+  $set->get_pareto;     
 
 =head1 DESCRIPTION
 
-Stub documentation for Data::Pareto, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
+This simple module makes calculation of Pareto set. Given a set of vectors
+(i.e. arrays of simple scalars), Pareto set is all the vectors from the given
+set which are not dominated by any other vector of the set. A vector C<X> is
+said to be dominated by C<Y>, iff C<< X[i] >= Y[i] >> for all C<i> and C<X[i] < Y[i]> for
+at least one C<i>.
 
-Blah blah blah.
+Pareto sets play an important role in multiobjective optimization, where
+each non-dominated (i.e. Pareto) vector describes objectives value of
+"optimal" solution to the given problem.
 
 =head2 EXPORT
 
-None by default.
-
-
+None.
 
 =head1 SEE ALSO
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
+TODO
 
 =head1 AUTHOR
 
-Przemyslaw Wesolek, E<lt>jest@E<gt>
+Przemyslaw Wesolek, <jest@go.art.pl>
 
 =head1 COPYRIGHT AND LICENSE
 
@@ -82,6 +166,5 @@ Copyright (C) 2009 by Przemyslaw Wesolek
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.10.0 or,
 at your option, any later version of Perl 5 you may have available.
-
 
 =cut
--- t/Data-Pareto.t
+++ t/Data-Pareto.t
@@ -1,17 +1,87 @@
-# Before `make install' is performed this script should be runnable with
-# `make test'. After `make install' it should work as `perl Data-Pareto.t'
+use Test::More 'no_plan';
 
-#########################
-
-# change 'tests => 1' to 'tests => last_test_to_print';
-
-use Test;
-BEGIN { plan tests => 1 };
 use Data::Pareto;
-ok(1); # If we made it this far, we're ok.
 
-#########################
+# a helper method to calculate Pareto set from given vectors
+sub _p {
+	my $num = shift;
+	my $opts = { };
+	$opts = shift if @_ && ref($_[0]) eq 'HASH';
+	my $p = new Data::Pareto({ cols => [ 0..$num-1 ], %$opts });
+	$p->addAll([ @_ ]) if @_;
+	return $p->getPareto();
+}
 
-# Insert your test code below, the Test::More module is use()ed here so read
-# its man page ( perldoc Test::More ) for help writing this test script.
+# same as above, assuming duplicates are allowed
+sub _p_dup {
+	my $num = shift;
+	return _p($num, { duplicates => 1 }, @_);
+}
 
+##### simple, <1 element sets
+
+is_deeply (
+	_p(2),
+	[ ]
+);
+
+is_deeply (
+	_p(2, [1,2]),
+	[ [1,2] ]
+);
+
+is_deeply (
+	_p(2, [1,2], [1,2]),
+	[ [1,2] ]
+);
+
+is_deeply (
+	_p_dup(2, [1,2], [1,2]),
+	[ [1,2], [1,2] ]
+);
+
+##### simple, 2 element sets of different column values
+
+is_deeply (
+	_p(2, [1,2], [1,3]),
+	[ [1,2] ]
+);
+
+is_deeply (
+	_p(2, [1,2], [2,2]),
+	[ [1,2] ]
+);
+
+is_deeply (
+	_p(2, [1,3], [1,2]),
+	[ [1,2] ]
+);
+
+is_deeply (
+	_p(2, [2,2], [1,2]),
+	[ [1,2] ]
+);
+
+##### adding element, removing element, tried to add again; in different confs.
+
+is_deeply (
+	_p(2, [2,2], [1,2], [2,2]),
+	[ [1, 2] ]
+);
+
+is_deeply (
+	_p_dup(2, [2,2], [2,2], [1,2]),
+	[ [1,2] ]
+);
+
+is_deeply (
+	_p_dup(2, [1,2], [2,2], [1,2], [2,2]),
+	[ [1,2], [1,2] ]
+);
+
+##### many pareto vectors
+
+is_deeply (
+	_p(3, [1,2,9], [2,2,8], [3,3,7], [4,3,6], [5,7,5]),
+	[ [1,2,9], [2,2,8], [3,3,7], [4,3,6], [5,7,5] ]
+);
